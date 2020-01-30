bf422d10676f5c84506d4b3801416b3bf71bcdb1
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-01-31 02:25:12 +0900

    reload input field on each new video page

--- skipAd.js
+++ skipAd.js
@@ -4,6 +4,7 @@
 const containerBox = document.createElement("input");
 containerBox.setAttribute("id", "skipAdBox");
 containerBox.setAttribute("value", "PRESS ENTER HERE");
+
 document.getElementById("buttons").appendChild(containerBox);
 
 const input = document.getElementById("skipAdBox");
@@ -13,3 +14,22 @@ input.addEventListener("keyup", function(event) {
     document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0].click();
   }
 });
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementsByTagName("h1")[0];
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function() {
+  setTimeout(function() {
+    document.getElementById("buttons").appendChild(containerBox);
+  }, 500);
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
