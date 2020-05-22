eb84cdccf4fe9f65ca80ed3c889b139c016cc282
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-05-22 11:25:56 +0900

    remove linkedin logo from navbar

--- linkedInNavbar.js
+++ linkedInNavbar.js
@@ -4,9 +4,10 @@
 
 const feedIcon = document.getElementById("feed-nav-item");
 const notifyIcon = document.getElementById("notifications-nav-item");
+const logo = document.getElementById("inbug-nav-item");
 
 function hideElements(...elements) {
   elements.forEach((element) => (element.style.display = "none"));
 }
 
-hideElements(feedIcon, notifyIcon);
+hideElements(feedIcon, notifyIcon, logo);
