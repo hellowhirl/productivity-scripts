2ac2104a65fd74926eb2f014539524926bf8dc23
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-10-02 11:01:01 +0900

    scipt to enlarge text input on classic.jisho

---
+++ classJisho.js
@@ -0,0 +1,11 @@
+//////
+// Make text input bigger for classic.jisho
+//
+
+const jField = document.getElementById("jap_field");
+const eField = document.getElementById("eng_field");
+const searchArea = document.getElementsByClassName("search")[0];
+
+jField.style.fontSize = "16px";
+eField.style.fontSize = "16px";
+searchArea.style.backgroundColor = "ghostwhite";
